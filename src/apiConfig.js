8df81edcc4ca7be04d078fe037cef35044a64abe
jordandickow <jdickow96@gmail.com>
2019-08-23 15:00:03 -0400

    switched default URL with heroku app  -  emptied README

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
-  development: 'http://localhost:4741'
+  production: 'https://serene-retreat-64737.herokuapp.com',
+  development: 'http://localhost:7165'
 }
 
 if (window.location.hostname === 'localhost') {

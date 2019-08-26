95d1af9ca2855aa306cde05f9beefe31ca3e7812
jordandickow <jdickow96@gmail.com>
2019-08-26 11:17:50 -0400

    fixed localhost issue  (connected api to  client / client to api port )

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
   production: 'https://serene-retreat-64737.herokuapp.com',
-  development: 'http://localhost:7165'
+  development: 'http://localhost:4741'
 }
 
 if (window.location.hostname === 'localhost') {
